0267400109f41bb0b0f724aeb57e55e4ec6ef438
Arun Jois <arun.joiss@gmail.com>
2019-04-30 17:43:01 +0530

    Partially Working

--- QuickSort.java
+++ QuickSort.java
@@ -1,35 +1,44 @@
 public class QuickSort {
-	public static void Quick(int a[],int start,int end) {
+	public void Quick(int a[],int start,int end) {
 		if(start<end) {
 			int pIndex = Partition(a,start,end);
 			Quick(a,start,pIndex-1);
 			Quick(a,pIndex+1,end);
 		}
 	}
-	public static void swap(int a,int b){
+	public void swap(int a,int b){
 		int temp=a;
 		a=b;
 		b=temp;
 	}
-	public static int Partition(int a[],int start,int end) {
+	public int Partition(int a[],int start,int end) {
 		int pivot = a[end];
 		int pIndex = start;
-		for(int i=start;i<=end;i++) {
-			if(a[i]<=pivot-1) {
-				swap(a[i],a[pIndex]);
+		for(int i=start;i<end;i++) {
+			if(a[i]<pivot) {
+
+				//swap(a[i],a[pIndex]);
+				int temp=a[i+1];
+				a[i+1]=a[pIndex];
+				a[pIndex]=temp;
 				pIndex++;
 			}
 		}
-		swap(a[pIndex],a[end]);
+		//swap(a[pIndex+1],a[end]);
+		int temp=a[pIndex];
+		a[pIndex]=a[end];
+		a[end]=temp;
 		return pIndex;
 	}
 	public static void main(String args[]) {
-		int a[]={5,6,7,8,9,1,2,3,4};
+		QuickSort q = new QuickSort();
+		int a[]={5,4,8,7,3,6,1};
 		int end=a.length-1;
 		System.out.println("Array Before sorting");
 		for(int i=0;i<a.length;i++)
 			System.out.print(a[i]);
-		Quick(a,0,end);
+		System.out.println("\n"+end);
+		q.Quick(a,0,end);
 		System.out.println("\nArray After Sorting");
 		for(int i=0;i<a.length;i++)
 			System.out.print(a[i]);
